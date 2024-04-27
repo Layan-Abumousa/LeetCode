79173834a395ec742b479f01131adcbaf67e6f66
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 01:51:05 +0300

    1979. Find Greatest Common Divisor of Array

---
+++ 1979. Find Greatest Common Divisor of Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findGCD(int[] nums) {
+          Arrays.sort(nums);int gcd=1;
+        for (int i = 1; i <= nums[0]; i++) {
+            if (nums[0] % i == 0 && nums[nums.length-1] % i == 0) {
+                gcd = i; 
+            }
+        }
+        return gcd;
+    }
+}
